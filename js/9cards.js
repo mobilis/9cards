ecd9f819f3d5099fa1c51ea915319b66b1e54221
mcnesium <mcnesium@versteht.es>
2013-05-06 14:19:25 +0200

    load & store data

---
+++ js/9cards.js
@@ -0,0 +1,60 @@
+/* mobilis 9Cards
+ * 
+ */
+
+
+var m9cards = {
+
+	loadData : function(data) {
+		var loadedObjects = {};
+		$.each(data, function(index,value){
+			loadedObjects[value] = localStorage.getItem('mobilis.9cards.'+value);
+			console.log('loaded', value, ': ', loadedObjects[value]);
+		});
+		return loadedObjects;
+	},
+
+	storeData : function(storedObjects) {
+
+		$.each(storedObjects, function(index,value){
+			localStorage.setItem('mobilis.9cards.'+index, value);
+			console.log('stored', index, ': ', value);
+		});
+		return true;
+	}
+
+}
+
+
+
+
+
+
+/* jQuery Event Handlers */
+
+$(document).on('pageshow', '#settings-page', function() {
+
+	settingsData = m9cards.loadData(['username','gameserver','jid','password']);
+
+	$('#settings-form #username').val(settingsData.username);
+	$('#settings-form #gameserver').val(settingsData.gameserver);
+	$('#settings-form #jid').val(settingsData.jid);
+	$('#settings-form #password').val(settingsData.password);
+
+	return true;
+});
+
+
+
+
+$(document).on('vclick', '#settings-form #submit', function() {
+
+	m9cards.storeData({
+		'username': 	$('#settings-form #username').val(),
+		'gameserver': 	$('#settings-form #gameserver').val(),
+		'jid': 			$('#settings-form #jid').val(),
+		'password': 	$('#settings-form #password').val()
+	});
+
+	return true;
+});
