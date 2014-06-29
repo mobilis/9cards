994c67939d85255a1808b7b60cb700f844fcea4a
Markus Wutzler <markus.wutzler@tu-dresden.de>
2014-06-29 16:25:06 +0200

    cleaned up game list a bit

--- MobilisNineCards_iOS/NineCards/NineCards/GameListTableViewController.m
+++ MobilisNineCards_iOS/NineCards/NineCards/GameListTableViewController.m
@@ -82,11 +82,12 @@
 
 - (void)handleRefresh:(UIRefreshControl *)sender
 {
+	self.availableGames = nil;
+	[self.tableView reloadData];
 	[[MXiConnectionHandler sharedInstance].serviceManager rediscoverServices];
 	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 10000000000), dispatch_get_main_queue(), ^{
 		[self.refreshControl endRefreshing];
 	});
-	self.availableGames = [MXiConnectionHandler sharedInstance].serviceManager.services;
 }
 
 #pragma mark - MXiServiceManagerDelegate
